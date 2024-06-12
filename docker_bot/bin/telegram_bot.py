19e40978d170055beeb5208191eac68ad7aacd60
Lorenzo <lorenzodidome03@gmail.com>
2024-06-12 15:29:06 +0200

    feat: design of the application

--- docker_bot/bin/telegram_bot.py
+++ docker_bot/bin/telegram_bot.py
@@ -31,17 +31,24 @@ teams_dict = { "Inter": 505,
          "Salernitana": 514
   }
 
+MENU = " Command list:  \n \
+- /analyze \nTo analyze and compare exepected Serie A standings from 2010 to 2023 \n \
+- /analyze \"year\" \nTo analyze and compare exepected Serie A standing of the year \n \
+- /predict \"year\" \"team\" \n To predict position of a Serie A team with random stats \n \
+- /predict \"year\" \"team\" \"gf\" \"gs\" \"win\" \"draw\" \"lose\" \n To predict position of a Serie A team with some stats \n \
+- /simulate \"year\" To simulate the standing of the year with random stats \n"
 
+ELABORATED = "Results Elaborated. See it on Kibana "
+
+ERROR = "Error in the parameters. Command /info for help"
+
+#per rispondere con le funzionalità possibili del bot
 async def info_function(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-   message = "Command list: \n \
-               - /analyze \n  To analyze and match predict Serie A standings from 2010 to 2023 \n \
-               - /analyze \"year\" \n To analyze and match predict Serie A standing of the year \n \
-               - /predict \"year\" \"team\" \n To predict position of a Serie A team with random stats \n \
-               - /predict \"year\" \"gf\" \"gs\" \"win\" \"draw\" \"lose\" \n To predict position of a Serie A team with some stats "
-   await update.message.reply_text(message)
+   global MENU
+   await update.message.reply_text(MENU)
 
 
-#per analizzare e fare predizioni dal 2011 al 2023
+#per analizzare e fare predizioni dal 2011 al 2024
 async def total_analyze_function(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     
    global teams_dict
@@ -58,109 +65,40 @@ async def total_analyze_function(update: Update, context: ContextTypes.DEFAULT_T
    
    if(len(words) == 2):
       year = words[1]
-   # if (words[1]!="Bundesliga"):
-   #    word = words[1]+" "+words[2] # team = words[3] 
-   # else:
-    #   word = words[1]
-      # team = words[2]
-
    
-   # id_team = teams_dict[team]
-
-   id = 135
-   #word = word.strip()
-  
-   # if(word=="Serie A"):
-   #    id=135
-   # elif (word=="Ligue 1"):
-   #    id=61
-   # elif(word=="Premier League"):
-   #    id=39
-   # elif(word=="Bundesliga"):
-   #    id=78
-   # elif(word=="La Liga"):
-   #    id=140
-   # else:
-   #    await update.message.reply_text("Input League Error. Insert Serie A ")
-   #   return
+   id = 135    #id della serie A 
+   
  
- 
-    #url = "https://v3.football.api-sports.io/standings?league=135&season=202"
-
-   if(year is None): 
-      for i in range(2011,2024):
+   if(year is None):    #se l'anno non è stato passato faccio analisi dal 2011 al 2024
+      for i in range(2011,2025):
          if i != 2011:
-            time.sleep(5)  #tra una richiesta e l\"altra si ferma 6 secondi
+            time.sleep(5)  #tra una richiesta e l\"altra si ferma 5 secondi
  
-         url = str(os.environ["URL"])+"league="+str(id)+"&season="+str(i)
-         #+"&team="+str(id_team)
+         url = str(os.environ["URL"])+"league="+str(id)+"&season="+str(i)   #url = "https://v3.football.api-sports.io/standings?league=135&season2023"
          response = requests.request("GET", url, headers=headers, data=payload)
+         json_object = json.loads(response.text)  
+         r = requests.request("POST",url='http://logstash:8080',json=json_object)  
 
-         json_object = json.loads(response.text)  #questo lo devo inviare a logstash
-         r = requests.request("POST",url='http://10.0.100.22:8080',json=json_object)  #indirizzo ip di logstash nella rete dei container
-
-         if i == 2023:
-            await update.message.reply_text("Result elaborated. See it on http://localhost:5601")
+         if i == 2024:
+            await update.message.reply_text(ELABORATED)
          else:
             await update.message.reply_text("Elaborating...")
-   else:
+   elif (int(year) <2010 or int(year)>2025):
+      await update.message.reply_text(ERROR)
+   else:   #faccio analisi per l'anno prestabilito
       url = str(os.environ["URL"])+"league="+str(id)+"&season="+str(year)
-      #+"&team="+str(id_team)
       response = requests.request("GET", url, headers=headers, data=payload)
-
       json_object = json.loads(response.text)  #questo lo devo inviare a logstash
-      r = requests.request("POST",url="http://10.0.100.22:8080",json=json_object)  #indirizzo ip di logstash nella rete dei container
 
+      r = requests.request("POST",url="http://logstash:8080",json=json_object)  
+      time.sleep(5)
+      await update.message.reply_text(ELABORATED)
    
-      await update.message.reply_text("Result elaborated. See it on http://localhost:5601")
-   
-
    message = ""
    url = ""
 
-
-async def predict_random_function(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-
-       #devo creare un json come quello che arriva a kafka e e su quello lavorerà spark 
-       #lì mi arriva testo e lo faccio in json con load e lo mando a logstash 
-      global teams_dict 
-
-      message = update.message.text     #il messaggio  è predict nomeSquadra anno(\"points\" \"gf\" \"gs\" \"win\" \"draw\" \"lose\")
-      words = message.split()
-      
-      team=year=points=win=lose=draw=gol_for=gol_against =  None
- 
-      if(len(words)< 3 or (len(words)>3 and len(words)!= 8) or(len(words) > 8)):
-         await update.message.reply_text("Error in the parameters. Type /info for help ")
-         return
-      elif(len(words) == 3):
-         team =  words[1]
-         year = words[2]
-
-        
-         while True:
-            win = random.randrange(0,39)  #per essere sicuro che il numero di partite perse, vinte  o pareggiato sia 38
-            lose = random.randrange(0,39)
-            draw = random.randrange(0,39)
-            if (win+lose+draw == 38):
-               break
-      
-         gol_for = random.randrange(10,100)
-         gol_against = random.randrange(0,100)
-         points = win*3+draw*1
-
-      else:
-         team =  words[1]
-         year = words[2]
-         gol_for = words[3]
-         gol_against = words[4]
-         win = words[5]
-         lose = words[6]
-         draw = words[7]
-         points = int(win)*3+int(draw)*1
-      
-       #mi creo quindi una stringa con dati random e lo trasformo in json e glielo mando 
-      response = "{    \
+def generate_json(year,team,points,win,draw,lose,gol_for,gol_against):
+   response =  "{    \
                 \"parameters\": { \
                      \"season\": \" " + year +" \" \
                   }, \
@@ -189,16 +127,91 @@ async def predict_random_function(update: Update, context: ContextTypes.DEFAULT_
                            } \
                            } ] \
                         }"
+   return response
+         
+#funzione che predice una posizione calcolando statistiche random sul club chiesto
+async def predict_random_function(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+
+      global teams_dict 
+
+      message = update.message.text     #il messaggio  è predict nomeSquadra anno(\"gf\" \"gs\" \"win\" \"draw\" \"lose\")
+      words = message.split()
+      
+      team=year=points=win=lose=draw=gol_for=gol_against =  None
+ 
+      if(len(words)< 3 or (len(words)>3 and len(words)!= 8) or(len(words) > 8)):
+         await update.message.reply_text(ERROR)
+         return 
+      elif(len(words) == 3):
+         team =  words[1]
+         year = words[2]
+
+        
+         while True:   #per essere sicuro che il numero di partite perse, vinte  o pareggiato sia 38
+            win = random.randrange(0,39) 
+            lose = random.randrange(0,39)
+            draw = random.randrange(0,39)
+            if (win+lose+draw == 38):
+               break
+      
+         gol_for = random.randrange(0,100)
+         gol_against = random.randrange(0,100)
+         points = win*3+draw*1
+
+      else:
+         team =  words[1]
+         year = words[2]
+         gol_for = words[3]
+         gol_against = words[4]
+         win = words[5]
+         lose = words[6]
+         draw = words[7]
+         points = int(win)*3+int(draw)*1
+      
+      #mi creo qui una stringa con dati random e lo trasformo in json e glielo mando 
+      response = generate_json(year,team,points,win,draw,lose,gol_for,gol_against)
 
       json_object = json.loads(response)  #questo lo devo inviare a logstash
-      r = requests.request("POST",url="http://10.0.100.22:8080",json=json_object)  #indirizzo ip di logstash nella rete dei container
+      r = requests.request("POST",url="http://logstash:8080",json=json_object)  #indirizzo ip di logstash nella rete dei container
 
+      await update.message.reply_text(ELABORATED)
    
-      await update.message.reply_text(" Result elaborated. See it on <a href='https://localhost:5601'> kibana </a> ",parse_mode = ParseMode.HTML)
-   
 
+async def simulate_year_function(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+      global teams_dict 
 
+      message = update.message.text     #il messaggio  è \simulate year
+      words = message.split()
+      year = None
 
+      if(len(words) >= 2):
+         year = words[1]
+      else: 
+         year = "2025"
+
+      for key in teams_dict:   #per ogni squadra nel dizionario 
+         points=win=lose=draw=gol_for=gol_against =  None
+         team = key
+
+         while True:   #per essere sicuro che il numero di partite perse, vinte  o pareggiato sia 38
+            win = random.randrange(0,39) 
+            lose = random.randrange(0,39)
+            draw = random.randrange(0,39)
+            if (win+lose+draw == 38):
+               break
+         gol_for = random.randrange(0,100)
+         gol_against = random.randrange(0,100)
+         points = win*3+draw*1
+
+         #mi creo qui una stringa con dati random e lo trasformo in json e glielo mando 
+         response = generate_json(year,team,points,win,draw,lose,gol_for,gol_against)
+         json_object = json.loads(response)  #questo lo devo inviare a logstash
+         r = requests.request("POST",url="http://logstash:8080",json=json_object)  #indirizzo ip di logstash nella rete dei container
+
+         time.sleep(3)
+         await update.message.reply_text("Elaborated Stats for "+key)
+
+      await update.message.reply_text(ELABORATED)
 
 token = os.environ["TOKEN"]    #token api telegram bot
 app = ApplicationBuilder().token(token).build()
@@ -206,5 +219,7 @@ app = ApplicationBuilder().token(token).build()
 app.add_handler(CommandHandler("analyze",total_analyze_function))
 app.add_handler(CommandHandler("info",info_function))
 app.add_handler(CommandHandler("predict",predict_random_function))
+app.add_handler(CommandHandler("simulate",simulate_year_function))
+
 app.run_polling()
 
